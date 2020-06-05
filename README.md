fa50af48662284f4ff99057ecf16a5549741c7eb
Kyle Williams <kwilliams@peakactivity.com>
2020-06-05 18:27:38 -0400

    init

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# grammerhub-web-strapi
+# Strapi application
+
+A quick description of your strapi application
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '0b4afab6-5d7c-4de7-bc2c-cc0aae457e94'
+};
